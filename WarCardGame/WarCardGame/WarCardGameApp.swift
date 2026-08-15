fac7acd97cb5972d2bc14895e5a456565da5b891
Simon McGuirk <5030092+simonmcguirk@users.noreply.github.com>
2026-08-15 10:24:36 +0100

    Create project

---
+++ WarCardGame/WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Simon McGuirk on 15/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
